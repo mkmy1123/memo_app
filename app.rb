dfd469decd39fa5c6da1832ef718f3bd6952ef40
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-10-05 17:42:12 +0900

    implement -> view show / method delete

--- app.rb
+++ app.rb
@@ -20,11 +20,36 @@ post "/memo" do
   redirect '/'
 end
 
+get "/memo/:id" do
+  get_memo(params[:id])
+  erb :show
+end
+
+delete "/memo/:id/delete" do
+  deleted = all_memo.delete_if { |memo| p memo["id"] == params[:id].to_i }
+  File.open(FILE_PATH, "w", 0755) do |file|
+    deleted.map! do |memo|
+      json = memo.to_json
+      file.puts json
+    end
+  end
+  redirect '/'
+end
+
+def get_memo(id)
+  id = id.to_i
+  all_memo.each do |memo|
+    if memo["id"] == id
+      @memo = memo
+    end
+  end
+end
+
 def create(title, body)
   File.open(FILE_PATH, "a", 0755) do |file|
-    id = all_memo.last["id"] + 1
+    id = all_memo.none? ? 1 : all_memo.last["id"] + 1
     json =<<~JSON
-    { "id": #{id}, "title": "#{title}", "body": "#{body}" }
+    { "id": #{id}, "title": "#{title}", "body": #{body.dump} }
     JSON
     file.puts json
   end
