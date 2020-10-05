7db8455c5cc16890e5924673d3de806fd83b319d
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-10-05 13:14:14 +0900

    implement new -> create

--- README.md
+++ README.md
@@ -6,8 +6,5 @@
 $ bundle install
 ```
 ``` bash
-$ sass --sourcemap=none sass/mystyles.scss:public/css/styles.css
-```
-``` bash
 $ bundle exec ruby app.rb
 ```
--- app.rb
+++ app.rb
@@ -3,5 +3,32 @@ require 'sinatra/reloader'
 
 # root page
 get "/" do
+  @memos = all_memo("memo")
   erb :root
+end
+
+get "/new" do
+  erb :new
+end
+
+post "/memo" do
+  create(params[:title], params[:body])
+  redirect '/'
+end
+
+def create(title, body)
+  File.open("memo/#{title}", "w", 0755) { |f| f.print body }
+end
+
+def all_memo(folder_name)
+  memos = []
+  Dir.open(folder_name).each do |file|
+    unless file[0] == "."
+      memos << {
+        title: file,
+        body: file
+      }
+    end
+  end
+  memos
 end
