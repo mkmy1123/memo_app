c56a1e3dcb3efdbbc0b7cea07caa188d86ec0764
Maki Miyagawa <61111655+mkmy1123@users.noreply.github.com>
2020-10-06 14:39:43 +0900

    refoctoring by myself

--- app.rb
+++ app.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
-
 require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 require 'date'
 
-# root page
+# --- DB CONFIG ---
+configure do
+  set :db_connect, PG.connect(
+    user: 'postgres', dbname: 'memo_db'
+  )
+end
+
+# ---- ROUTING -----
 get '/' do
   @memos = get_all_memo
   erb :root
@@ -21,18 +27,17 @@ post '/memo' do
 end
 
 get '/memo/:id' do
-  get_memo(params[:id])
+  @memo = get_memo(params[:id])
   get_html_body(@memo)
   erb :show
 end
 
 get '/memo/:id/edit' do
-  get_memo(params[:id])
+  @memo = get_memo(params[:id])
   erb :edit
 end
 
 patch '/memo/:id/edit' do
-  get_memo(params[:id])
   update(params[:id], params[:title], params[:body])
   redirect "/memo/#{params[:id]}"
 end
@@ -48,8 +53,7 @@ def create(title, body)
     INSERT INTO memo(id, title, body, updated_at, created_at)
     VALUES(DEFAULT, $1, $2, now(), now())
   SQL
-  conn = PG.connect( dbname: 'memo_db', user: 'postgres' )
-  conn.exec(sql, [title, body])
+  setting.db_connect.exec(sql, [title, body])
 end
 
 def update(id, title, body)
@@ -60,36 +64,26 @@ def update(id, title, body)
         updated_at = now()
     WHERE id = #{id}
   SQL
-  conn = PG.connect( dbname: 'memo_db', user: 'postgres' )
-  conn.exec(sql, [title, body])
+  settings.db_connect.exec(sql, [title, body])
 end
 
 def delete(id)
-  conn = PG.connect( dbname: 'memo_db', user: 'postgres' )
-  conn.exec( "DELETE FROM memo WHERE id = #{id}" )
+  settings.db_connect.exec( "DELETE FROM memo WHERE id = #{id}" )
 end
 
 # ---- GET DATA'S METHOD -----
 def get_all_memo
-  conn = PG.connect( dbname: 'memo_db' )
-  memos = []
-  conn.exec( "SELECT * FROM memo" ) do |result|
-    result.each do |row|
-      memos << row
-    end
-  end
-  memos
+  settings.db_connect.exec( "SELECT * FROM memo" )
 end
 
 def get_memo(id)
-  conn = PG.connect( dbname: 'memo_db' )
-  conn.exec( "SELECT * FROM memo WHERE id = #{id}" ) do |result|
-    result.each do |row|
-      @memo = row
-    end
-  end
+  sql = <<~SQL
+    SELECT * FROM memo WHERE id = #{id}
+  SQL
+  settings.db_connect.exec(sql).first
 end
 
+# ---- ARRANGE DATA ----
 def get_html_body(memo)
   array = memo['body'].lines.map do |line|
     if line == "\r\n"
